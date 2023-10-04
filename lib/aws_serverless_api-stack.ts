948fa84f22a5f35790a77005c0f819af6adbac4b
jayarajmadanu <jayarajmadanu@gmail.com>
2023-10-04 17:17:46 +0530

    first commit

--- lib/aws_serverless_api-stack.ts
+++ lib/aws_serverless_api-stack.ts
@@ -1,5 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsServerlessApiStack extends cdk.Stack {
@@ -8,9 +12,43 @@ export class AwsServerlessApiStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsServerlessApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //Dynamo Table
+    const todoTable = new Table(this, "todo", {
+      partitionKey:{name:"name", type:AttributeType.STRING}
+    });
+    
+    // lambda function to GetAllTodosLambdaHandler
+    const getAllTodosLambda = new Function(this, "GetAllTodosLambdaHandler", {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset("functions"),
+      handler: "get-all-todos.getAllTodosHandler",
+      environment: {
+        TODO_TABLE_NAME: todoTable.tableName,
+      },
+    });
+
+    //give permission to lambda to access todoTable
+    todoTable.grantReadWriteData(getAllTodosLambda)
+
+    // lambda to write todo item into db
+    const putTodoLambda = new Function(this, "PutTodoLambda", {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset("functions"),
+      handler: "put-todo.putTodosHandler",
+      environment: {
+        TODO_TABLE_NAME: todoTable.tableName,
+      }
+    });
+
+    todoTable.grantReadWriteData(putTodoLambda)
+
+
+    const api = new RestApi(this, "todo-api");
+    api.root.resourceForPath("todo").addMethod("GET", new LambdaIntegration(getAllTodosLambda))
+    api.root.resourceForPath("todo").addMethod("POST", new LambdaIntegration(putTodoLambda))
+
+    new cdk.CfnOutput(this,"API-URL", {
+      value: api.url ?? "Something Went Wrong"
+    });
   }
 }
